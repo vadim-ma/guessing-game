24a4f4ac9c444b7815ad0639bd08a83607e98e01
vadim-ma <color.ui>
2023-10-01 22:26:51 +0300

    feat: solve the problem

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    }
+    
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.ceil( (this.max + this.min) / 2 );
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
-
+    
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
